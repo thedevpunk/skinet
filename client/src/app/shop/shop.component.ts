e43e93f154207bd52d34216218be1ac6fe8654a5
thedevpunk <andrelindemail@gmail.com>
2020-06-08 22:19:25 +0200

    Intermediate status 2 section 9

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -13,6 +13,8 @@ export class ShopComponent implements OnInit {
   products: IProduct[];
   brands: IBrand[];
   types: IType[];
+  brandIdSelected: number = 0;
+  typeIdSelected: number = 0;
 
   constructor(private shopService: ShopService) { }
 
@@ -23,7 +25,7 @@ export class ShopComponent implements OnInit {
   }
 
 getProducts() {
-  this.shopService.getProducts().subscribe(response => {
+  this.shopService.getProducts(this.brandIdSelected, this.typeIdSelected).subscribe(response => {
     this.products = response.data;
   }, error => {
     console.log(error);
@@ -32,7 +34,7 @@ getProducts() {
 
 getBrands() {
   this.shopService.getBrands().subscribe(response => {
-    this.brands = response;
+    this.brands = [{id: 0, name: 'All'}, ...response];
   }, error => {
     console.log(error);
   });
@@ -40,10 +42,20 @@ getBrands() {
 
 getTypes() {
   this.shopService.getTypes().subscribe(response => {
-    this.types = response;
+    this.types = [{id: 0, name: 'All'}, ...response];
   }, error => {
     console.log(error);
   });
+}
+
+onBrandSelected(brandId: number){
+  this.brandIdSelected = brandId;
+  this.getProducts();
+}
+
+onTypeSelected(typeId: number){
+  this.typeIdSelected = typeId;
+  this.getProducts();
 }
 
 }
